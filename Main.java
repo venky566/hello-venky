e862ca62f3851ad0843e0a2e2bf6adc80a70dd13
venky <nallamotuvenkatesh9@gmail.com>
2022-01-14 20:14:25 -0600

    final

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void  main(String[] args) {
-        
+       JFrame window = new JFrame();
+       window.setSize(200, 400);
+       window.setLocation(300, 300);
+
+       window.pack();
+       window.setVisible(true);
     }
 }
